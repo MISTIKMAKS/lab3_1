51e2606b38fb4d50f6a5bc7bf932f4eb43a3358c
MISTIKMAKS <mistikmaks80885@gmail.com>
2020-11-05 02:47:17 +0200

    first master commit

---
+++ pr3_1/pr3_1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x; // вхідний параметр
+	double y; // результат обчислення виразу
+	double A; // проміжний результат - функціонально стала частина виразу
+	double B; // проміжний результат - функціонально змінна частина виразу
+	cout << "x = "; cin >> x;
+	A = (2+x)/(x*x)+1;
+	// спосіб 1: розгалуження в скороченій формі
+	if (x < 0)
+		B = pow(x, 3) - 2 * pow(x, 4);
+	if (x <= 0 && x >= 2)
+		B = pow(abs(x) + exp(x), 3);
+	if (x > 2)
+		B = 4 * cos((x * x) - 2);
+	y = A + B;
+	cout << endl;
+	cout << "1) y = " << y << endl;
+	// спосіб 2: розгалуження в повній формі
+	if (x < 0)
+		B = pow(x, 3) - 2 * pow(x, 4);
+	else
+		if (x > 2)
+			B = 4 * cos((x * x) - 2);
+		else
+			B = pow(abs(x) + exp(x), 3);
+	y = A - B;
+	cout << "2) y = " << y << endl;
+	cin.get();
+	return 0;
+}
